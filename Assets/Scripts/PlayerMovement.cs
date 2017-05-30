b85b072c5090a1e34b0239e6adb5c18bff1651f6
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-05-30 16:28:45 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed; //How fast the player will move
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * MovementSpeed; //horizontal force being applied to the player
+        float v = Input.GetAxis("Vertical") * MovementSpeed; //vertical force being applied to the player
+
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(h,0,v)); //Applies the Horizontal and Vertical force to the player's Rigidbody
+    }
+}
