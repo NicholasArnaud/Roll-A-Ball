c90dc20c01c9532dacb5fbaaf25f0d822d245cb7
Dylan Guidry <dylan.guidry@aie.edu.au>
2017-10-06 15:45:29 -0500

    Pick up

---
+++ Assets/Scripts/PickUpDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpDestroy: MonoBehaviour
+{
+    public float DestroyDelay;
+    bool BeingDestroyed = false;
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Check to make sure the object that has come inside the trigger value was the player
+        if (other.tag == "Player")
+        {
+            Destroy(this.GetComponent<CapsuleCollider>());
+            BeingDestroyed = true;
+        }
+    }
+}
