badcf84322f76d9f021552d1d928ed4a8f02457d
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-10-12 17:55:34 -0500

    Test on Teleportation

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    TeleporterController telecon;
+    public string Key;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            GameObject otherTele = telecon.GetLink(gameObject);
+            other.transform.position = otherTele.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/TeleporterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterController : MonoBehaviour
+{
+    GameObject[] teleporters;
+    // Use this for initialization
+    void Start()
+    {
+        List<string> keyLog = new List<string>();
+        teleporters = GameObject.FindGameObjectsWithTag("Teleporter");
+        foreach (var a in teleporters)
+        {
+            keyLog.Add(a.GetComponent<Teleporter>().Key);
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+
+    public GameObject GetLink(GameObject teleporter)
+    {
+        foreach(var a in teleporters)
+        {
+            if (teleporter.GetComponent<Teleporter>().Key == a.GetComponent<Teleporter>().Key)
+            {
+                return a.gameObject;
+            }
+        }
+        return null;
+    }
+
+}
