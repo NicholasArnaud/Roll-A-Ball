7d830d852717aa4c845ad06b9d6a0ad55d3306e3
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-10-13 11:16:10 -0500

    Completed Teleporter

--- Assets/Scripts/TeleporterController.cs
+++ Assets/Scripts/TeleporterController.cs
@@ -4,32 +4,62 @@ using UnityEngine;
 
 public class TeleporterController : MonoBehaviour
 {
-    GameObject[] teleporters;
+    public List<Teleporter> teleporters;
     // Use this for initialization
     void Start()
     {
-        List<string> keyLog = new List<string>();
-        teleporters = GameObject.FindGameObjectsWithTag("Teleporter");
-        foreach (var a in teleporters)
+        foreach (var obj in GameObject.FindGameObjectsWithTag("Teleporter"))
+            teleporters.Add(obj.GetComponent<Teleporter>());
+
+        for (int i = 0; i < teleporters.Count; i++)
         {
-            keyLog.Add(a.GetComponent<Teleporter>().Key);
+            int numOfDiffKeys = 0;
+            int numOfSameKeys = 0;
+            for (int j = 0; j < teleporters.Count; j++)
+            {
+                if (teleporters[i].Key == teleporters[j].Key)
+                {
+                    numOfSameKeys++;
+                    if (numOfSameKeys > 2)
+                    {
+                        Destroy(teleporters[j].gameObject);
+                        teleporters.RemoveAt(j);
+                        numOfSameKeys = 2;
+                    }
+                }
+                else
+                {
+                    numOfDiffKeys++;
+                    if(numOfDiffKeys ==teleporters.Count -1)
+                    {
+                        Destroy(teleporters[i].gameObject);
+                        teleporters.RemoveAt(i);
+                    }
+                }
+            }
         }
     }
 
     // Update is called once per frame
     public void Update()
     {
-        
+
     }
 
 
     public GameObject GetLink(GameObject teleporter)
     {
-        foreach(var a in teleporters)
+        foreach (var a in teleporters)
         {
+            Debug.Log("active key" + teleporter.GetComponent<Teleporter>().Key + "otehr key" + a.GetComponent<Teleporter>().Key);
             if (teleporter.GetComponent<Teleporter>().Key == a.GetComponent<Teleporter>().Key)
             {
-                return a.gameObject;
+                Debug.Log(a.name + " " + teleporter.name);
+                if (teleporter.gameObject != a.gameObject)
+                {
+
+                    return a.gameObject;
+                }
             }
         }
         return null;
