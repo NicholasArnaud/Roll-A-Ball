b82f386463834e0debe625c5ef4bd2a9560dcbbd
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-10-13 11:16:36 -0500

    Door Script

---
+++ Assets/Scripts/SlidingDoorBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoorBehaviour : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float MovementSpeed;
+
+    [Range(1,10)]
+    public float TriggerDistance;
+
+    public bool IsOpen;
+
+    public Vector3 EndPos;
+    public Vector3 StartPos;
+
+    void Start()
+    {
+        StartPos = transform.position;
+        IsOpen = false;
+    }
+
+    void Update()
+    {
+        if (IsOpen)
+        {
+            transform.position = Vector3.Lerp(transform.position, EndPos, Time.deltaTime * MovementSpeed);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, StartPos, Time.deltaTime * MovementSpeed);
+        }
+    }
+    
+    public void OpenDoor()
+    {
+        IsOpen = true;
+    }
+
+    public void CloseDoor()
+    {
+        IsOpen = false;
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(EndPos, transform.localScale);
+    }
+#endif
+}
